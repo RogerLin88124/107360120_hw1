75931149848312c15bcc0b8dece7f2878de0ea72
RogerLin88124 <linyiwen88124@gmail.com>
2020-10-13 21:13:53 +0800

    first

---
+++ hw2_pack/hw2_P16.java
@@ -0,0 +1,12 @@
+package hw2_pack;
+
+public class hw2_P16 {
+
+	public static void main(String[] args) {
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava!");
+		System.out.println(123);
+
+	}
+
+}
---
+++ hw2_pack/hw2_P25.java
@@ -0,0 +1,11 @@
+package hw2_pack;
+
+public class hw2_P25 {
+
+	public static void main(String[] args) {
+	int num;
+	num=3;
+	System.out.println("變數num的值是"+num);
+	}
+
+}
